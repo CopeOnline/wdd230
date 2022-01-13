e08c222d3eebbb3a0a06454a29d136e9dfaad42d
CopeOnline <cop21004@byui.edu>
2022-01-12 21:31:24 -0700

    greep

--- lesson2/js/dynamicdates.js
+++ lesson2/js/dynamicdates.js
@@ -4,4 +4,4 @@ let year = d.getFullYear();
 document.querySelector(".currentyear").textContent += ` ${year}  |  Larry E. Copeland Jr  |  Idaho`;
 
 const lastupdate = document.querySelector(".lastupdated");
-lastupdate.textContent = `Last Updated: ${document.lastModified}`;
+lastupdate.textContent = `Document was last updated: ${document.lastModified}`;
