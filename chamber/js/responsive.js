7dee52405eaf2d8fa447a5caff6e79e08101e183
CopeOnline <cop21004@byui.edu>
2022-01-27 21:20:05 -0700

    date

--- chamber/js/responsive.js
+++ chamber/js/responsive.js
@@ -1,7 +1,13 @@
 const d = new Date();
 let year = d.getFullYear();
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+let day = weekday[d.getDay()];
+let monthname = month[d.getMonth()];
 
 document.querySelector(".currentyear").textContent += `${year}`;
 
 const lastupdate = document.querySelector(".lastupdated");
 lastupdate.textContent += `Last Updated: ${document.lastModified}`;
+
+document.querySelector(".todaysDate").textContent = `${day}, ${d.getDay()} ${monthname} ${year}`;
