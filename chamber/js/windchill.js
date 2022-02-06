2a9483a7cd104a79a50958554e64d3034cb56202
CopeOnline <cop21004@byui.edu>
2022-02-05 23:39:05 -0700

    finally

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,6 +1,24 @@
+const weatherIcon = document.querySelector(".wIcon")
+
+
 async function getWeather() {
     const response = await fetch("http://api.openweathermap.org/data/2.5/weather?q=Shoshone,ID,USA&units=imperial&APPID=da28ef0488cf8a1538d20c2db5897dd8");
     let data = await response.json()
-    return data;
+    console.log(typeof(data))
+    console.log(data.name)
+    let icon = data['weather'][0]['icon'];
+
+    weatherIcon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+
+    
     }
-getWeather().then(data => console.log(data));
+    
+getWeather();
+
+
+
+
+
+
+
+
