b4f0fc2e35eaff90274e7390614f295719d48963
CopeOnline <cop21004@byui.edu>
2022-02-07 19:50:31 -0700

    correction

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -9,28 +9,6 @@ async function getWeather() {
     const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Shoshone,ID,USA&units=imperial&APPID=da28ef0488cf8a1538d20c2db5897dd8");
     let data = await response.json()
 
-    let newTemp = data['main']['temp']
-    let icon = data['weather'][0]['icon'];
-    let desc = data['weather'][0]['description'];
-    let speed = data['wind']['speed']
-
-    weatherIcon.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-    weatherIcon.alt = `open weather map API icon: ${desc}`;
-    iconDesc.textContent = desc.toUpperCase()
-
-    currentTemp = Math.round(newTemp)
-    localtemp.textContent += `${currentTemp}`;
-
-    currentspeed = Math.round(speed)
-    wSpeed.textContent = `${currentspeed} mph`
-
-    if (newTemp <= 50 && speed > 3.0) {
-        let chill = 35.74 + (0.6215*newTemp) - (35.75*speed**0.16) + (0.4275*newTemp*speed**0.16)
-        chill = Math.round(chill)
-        wChill.textContent = `Feels like ${chill}`
-    }else {
-        console.log('variable out of range')
-    }
     return data
     }
     
@@ -42,4 +20,25 @@ console.log(weatherData)
 
 
 
+let newTemp = data['main']['temp']
+let icon = data['weather'][0]['icon'];
+let desc = data['weather'][0]['description'];
+let speed = data['wind']['speed']
 
+weatherIcon.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+weatherIcon.alt = `open weather map API icon: ${desc}`;
+iconDesc.textContent = desc.toUpperCase()
+
+currentTemp = Math.round(newTemp)
+localtemp.textContent += `${currentTemp}`;
+
+currentspeed = Math.round(speed)
+wSpeed.textContent = `${currentspeed} mph`
+
+if (newTemp <= 50 && speed > 3.0) {
+    let chill = 35.74 + (0.6215*newTemp) - (35.75*speed**0.16) + (0.4275*newTemp*speed**0.16)
+    chill = Math.round(chill)
+    wChill.textContent = `Feels like ${chill}`
+}else {
+    console.log('variable out of range')
+}
