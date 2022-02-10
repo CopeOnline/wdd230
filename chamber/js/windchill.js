8354b8f2d54738b2e11311469af974c91197ed69
CopeOnline <cop21004@byui.edu>
2022-02-09 23:16:04 -0700

    encapsulation

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -4,26 +4,38 @@ const localtemp = document.querySelector(".temp")
 const wSpeed = document.querySelector(".windSpeed")
 const wChill = document.querySelector(".windChill")
 
+let apiInfo = "https://api.openweathermap.org/data/2.5/weather?q=Shoshone,ID,USA&units=imperial&APPID=da28ef0488cf8a1538d20c2db5897dd8"
+
+let data, newTemp, speed;
 
 async function getWeather() {
-    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Shoshone,ID,USA&units=imperial&APPID=da28ef0488cf8a1538d20c2db5897dd8");
-    let data = await response.json()
+    const response = await fetch(apiInfo);
+    data = await response.json()
 
-    let newTemp = data['main']['temp']
+    return data
+
+}    
+
+function fillData(){
+    newTemp = data['main']['temp']
     let icon = data['weather'][0]['icon'];
     let desc = data['weather'][0]['description'];
-    let speed = data['wind']['speed']
+    speed = data['wind']['speed']
 
     weatherIcon.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
     weatherIcon.alt = `open weather map API icon: ${desc}`;
     iconDesc.textContent = desc.toUpperCase()
 
-    currentTemp = Math.round(newTemp)
+    let currentTemp = Math.round(newTemp)
     localtemp.textContent += `${currentTemp}`;
 
-    currentspeed = Math.round(speed)
+    let currentspeed = Math.round(speed)
     wSpeed.textContent = `${currentspeed} mph`
 
+    return newTemp, speed
+    }
+
+function calculateWindChill() {
     if (newTemp <= 50 && speed > 3.0) {
         let chill = 35.74 + (0.6215*newTemp) - (35.75*speed**0.16) + (0.4275*newTemp*speed**0.16)
         chill = Math.round(chill)
@@ -32,8 +44,8 @@ async function getWeather() {
         wChill.textContent = `Feels like ${newTemp}`
     }
     }
-    
-getWeather();
+  
+getWeather().then(fillData).then(calculateWindChill);
 
 
 
