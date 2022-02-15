a2e33b99e5b48a12e117514ce04443a2f990967c
CopeOnline <cop21004@byui.edu>
2022-02-14 22:57:59 -0700

    tuy

--- chamber/js/visits.js
+++ chamber/js/visits.js
@@ -5,11 +5,11 @@ let msPerDay = 86400000
 let today = Date.now();
 let numVisits = Number(localStorage.getItem("visits"));
 let lastVisit = localStorage.getItem('dateVisited');
-let daysBwtnVisits;
+let daysBwtnVisits, difference;
 
 if (numVisits !== 0) {
     localStorage.setItem("dateVisited", today);
-    let difference = today - lastVisit
+    difference = today - lastVisit
     daysBwtnVisits = Math.round(difference / msPerDay)
 }else {
     localStorage.setItem('visits', 1)
