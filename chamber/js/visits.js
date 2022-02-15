0f04ca7522bc9b5b4353959b7073020fd1e0e3a5
CopeOnline <cop21004@byui.edu>
2022-02-14 22:56:22 -0700

    pug

--- chamber/js/visits.js
+++ chamber/js/visits.js
@@ -1,25 +1,25 @@
 
 const visits = document.querySelector(".visitor");
 
+let msPerDay = 86400000
 let today = Date.now();
 let numVisits = Number(localStorage.getItem("visits"));
 let lastVisit = localStorage.getItem('dateVisited');
+let daysBwtnVisits;
 
 if (numVisits !== 0) {
     localStorage.setItem("dateVisited", today);
+    let difference = today - lastVisit
+    daysBwtnVisits = Math.round(difference / msPerDay)
 }else {
     localStorage.setItem('visits', 1)
     localStorage.setItem("dateVisited", today);
+    daysBwtnVisits = 0
+
 }
 
 numVisits++
 
 localStorage.setItem('visits', numVisits)
-
-let difference = today - lastVisit
-
-let msPerDay = 86400000
-
-let daysBwtnVisits = Math.round(difference / msPerDay)
 
 visits.textContent = daysBwtnVisits
