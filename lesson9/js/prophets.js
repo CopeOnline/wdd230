25e5d2292fc8cdc0a28d2376e33de4b9e6f0ef9d
CopeOnline <cop21004@byui.edu>
2022-02-26 22:53:03 -0700

    drum

---
+++ lesson9/js/prophets.js
@@ -0,0 +1,37 @@
+
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject); 
+
+    const prophets = jsonObject['prophets'];
+    console.log(prophets)
+    prophets.forEach(displayProphets);
+  });
+
+
+
+function displayProphets(prophet, i) {  // Create elements to add to the document
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let birthDate = document.createElement('p');
+    let birthPlace = document.createElement('p');
+    let image = document.createElement('img');
+      // Change the textContent property of the h2 element to contain the prophet's full name
+    h2.textContent = `${prophet.name} ${prophet.lastname}`;
+    birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+    birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+    image.setAttribute('src', prophet.imageurl);
+    // Add/append the section(card) with the h2 element
+    card.appendChild(h2);
+    card.appendChild(birthDate);
+    card.appendChild(birthPlace);
+    card.appendChild(image);
+    // Add/append the existing HTML div with the cards class with the section(card)
+    document.querySelector('div.cards').appendChild(card);
+  }
+
