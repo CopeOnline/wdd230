4e650263555705b5c0405dc5fa5d1224f4ed4cb3
CopeOnline <cop21004@byui.edu>
2022-03-05 11:55:55 -0700

    spot

--- chamber/js/spotlight.js
+++ chamber/js/spotlight.js
@@ -2,18 +2,7 @@ const requestFile = 'json/data.json';
 let value = false
 let website = ''
 let directory
-
-
-function getRandomInt(max) {
-    let int = []
-    for (let i = 0; i < 3; i++) {
-        ts = Math.floor(Math.random() * max);
-        int.push(ts)
-        console.log(ts)
-    }
-    console.log(int)
-    return int
-  }
+let spotlight = ['spotOne', 'spotTwo', 'spotThree']
 
 function fetchData (directory){
     fetch(requestFile)
@@ -28,12 +17,24 @@ function fetchData (directory){
   });
 }
 
+function getRandomInt(max) {
+    let int = []
+    for (let i = 0; i < 3; i++) {
+        spot = Math.floor(Math.random() * max);
+        if (int.includes(spot)) {
+            i--;
+        } else {    
+            int.push(spot);
+        }
+    }
+    return int
+  }
+
 function getSpotlightMembers(directory){
     const newDir = directory.filter(memberStatus);
-
     const int = getRandomInt(newDir.length)    
     
-    displaydirectory(newDir, int);
+    displaySpotlight(newDir, int, spotlight);
 }
 
 function displayCompanyWebsite(listing, value) {
@@ -49,17 +50,44 @@ function displayCompanyWebsite(listing, value) {
 return {website, value};
 }
 
-function displaydirectory(listing, int) {
-    let spotOne = listing[int[0]]
-    let spotTwo = listing[int[1]]
-    let spotThree = listing[int[2]]
+function displaySpotlight(listing, int, label) {
+    for (let i=0; i < label.length; i++) {
+        let spotNum= label[i];
+        let compNum = int[i]
+        
+        let container = document.createElement('div');
+        container.setAttribute('class', `${spotNum}`)
+        document.querySelector('div.placeholder').appendChild(container);
 
-    let website = displayCompanyWebsite(listing, value)
-    console.log(listing)
-    console.log(int[0])
-    console.log(spotOne)
-    console.log(spotTwo)
-    console.log(spotThree)
+        let compName = document.createElement('h2');
+        let image = document.createElement('img');
+        let address = document.createElement('p');
+        let hr = document.createElement('hr')
+        let phone = document.createElement('p');
+        let website = displayCompanyWebsite(listing[compNum], value);
+
+        compName.textContent = `${listing[compNum].name}`;
+        address.textContent = `${listing[compNum].address}`;
+        phone.textContent = `${listing[compNum].phone}`;
+        image.setAttribute('src', listing[compNum].logo);
+        image.setAttribute('alt', `${listing[compNum].name} logo`);
+        hr.setAttribute('stlye', "width:90%;text-align:center;")
+        // Add/append the section(card) with the h2 element
+        container.appendChild(compName);
+        container.appendChild(image);
+        container.appendChild(address);
+        container.appendChild(hr)
+        container.appendChild(phone);
+    
+        if (website.value == true) {
+            container.appendChild(website.website);
+
+        }else {
+            alternate = document.createElement('p');
+            alternate.innerHTML = `<em>Come in and see us!</em>`;
+            container.appendChild(alternate);
+        }
+    }
   }
 
 function memberStatus(dir){
