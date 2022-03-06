0cfb40fc5069b4cba215c9eda5d679fb1a54c64c
CopeOnline <cop21004@byui.edu>
2022-03-05 18:04:59 -0700

    weather busy work

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,19 @@
+
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks,Al,USA&units=imperial&APPID=da28ef0488cf8a1538d20c2db5897dd8"
+
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.querySelector('#current-temp').textContent = jsObject.main.temp;
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
+  });
+
+
+
